8e0eeae8a22769a6ffdd876c2534454c162c2302
Fletcher <fletcherc89@gmail.com>
2015-09-03 00:47:54 -0700

    Project stuff and basic movement code

---
+++ Assets/Watchlist/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : VoBehavior
+{
+    public float AccelerationDuration = 0.5f;
+    public float MaxSpeed = 1.0f;
+    public Vector2 Velocity = new Vector2();
+
+    void Start()
+    {
+        _acceleration = this.MaxSpeed / this.AccelerationDuration;
+    }
+
+    void Update()
+    {
+        Vector2 movementAxis = getMovementAxis();
+
+        float targetX = movementAxis.x * this.MaxSpeed;
+        float targetY = movementAxis.y * this.MaxSpeed;
+
+        float changeX = targetX - this.Velocity.x;
+        float changeY = targetY - this.Velocity.y;
+
+        if (changeX != 0 || changeY != 0)
+        {
+            float changeTotal = Mathf.Sqrt(Mathf.Pow(changeX, 2) + Mathf.Pow(changeY, 2));
+
+            if (changeX != 0)
+            {
+                float ax = Mathf.Abs(_acceleration * changeX / changeTotal);
+                this.Velocity.x = Mathf.Lerp(this.Velocity.x, targetX, ax * Time.deltaTime / Math.Abs(changeX));
+            }
+
+            if (changeY != 0)
+            {
+                float ay = Mathf.Abs(_acceleration * changeY / changeTotal);
+                this.Velocity.y = Mathf.Lerp(this.Velocity.y, targetY, ay * Time.deltaTime / Math.Abs(changeY));
+            }
+        }
+
+        this.transform.position = new Vector3(this.transform.position.x + this.Velocity.x * Time.deltaTime, 
+                                              this.transform.position.y,
+                                              this.transform.position.z + this.Velocity.y * Time.deltaTime);
+    }
+
+    /**
+     * Private
+     */
+    private float _acceleration;
+
+    private Vector2 getMovementAxis()
+    {
+        Vector2 movementAxis = new Vector2();
+
+        if (Input.anyKey)
+        {
+            // Construct movment axis from 4-directional keyboard input
+            float x = 0;
+            float y = 0;
+
+            if (Input.GetKey(KeyCode.W)) y += 1;
+            if (Input.GetKey(KeyCode.A)) x -= 1;
+            if (Input.GetKey(KeyCode.S)) y -= 1;
+            if (Input.GetKey(KeyCode.D)) x += 1;
+
+            if (y != 0 && x != 0)
+            {
+                x = Mathf.Sign(x) * 0.70710678f;
+                y = Mathf.Sign(y) * 0.70710678f;
+            }
+
+            movementAxis.x = x;
+            movementAxis.y = y;
+        }
+
+        else
+        {
+            // Use controller input
+
+        }
+
+        return movementAxis;
+    }
+}
---
+++ Assets/Watchlist/Scripts/VoBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/**
+ * As recommended by Asher Vollmer https://twitter.com/AsherVo/status/461579941159501824
+ */
+public class VoBehavior : MonoBehaviour
+{
+	public new GameObject gameObject { get { 
+        if (!_gameObject) _gameObject = base.gameObject;
+        return _gameObject; 
+    } }
+
+	public new Transform transform { get {
+			if (!_transform) _transform = base.transform;
+			return _transform;
+	} }
+
+	public new Renderer renderer { get {
+			if (!_renderer) _renderer = base.GetComponent<Renderer>();
+			return _renderer;
+	} }
+	
+	public SpriteRenderer spriteRenderer { get {
+			if (!_spriteRenderer) _spriteRenderer = base.GetComponent<SpriteRenderer>();
+			return _spriteRenderer;
+	} }
+
+    public BoxCollider2D boxCollider2D { get {
+            if (!_boxCollider2D) _boxCollider2D = base.GetComponent<BoxCollider2D>();
+            return _boxCollider2D;
+    } }
+
+    /**
+     * Private
+     */
+    private GameObject _gameObject;
+    private Transform _transform;
+    private Renderer _renderer;
+    private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _boxCollider2D;
+}
