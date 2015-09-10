24220937da822b1e878acad9752026485551e55d
Fletcher <fletcherc89@gmail.com>
2015-09-10 14:17:43 -0700

    TextAnimator blinking cursor works and is nicer overall

--- Assets/Watchlist/Scripts/TextAnimator.cs
+++ Assets/Watchlist/Scripts/TextAnimator.cs
@@ -7,12 +7,21 @@ public class TextAnimator : VoBehavior
     public int CharactersToAnimate = 1;
     public int CharactersPerSecond = 10;
     public int SwapsPerSecond = 20;
+    public bool ClearOnStart = true;
     public bool RunOnStart = true;
+    public bool EnableCursorBlink = false;
+    public string CursorText = "_";
+    public float CursorBlinkOnDuration = 0.5f;
+    public float CursorBlinkOffDuration = 0.2f;
 
     void Start()
     {
+        _textComponent = this.GetComponent<Text>();
+
         if (this.RunOnStart)
             this.Begin();
+        else if (this.ClearOnStart)
+            _textComponent.text = "";
     }
 
 	void Update()
@@ -38,7 +47,7 @@ public class TextAnimator : VoBehavior
 
                     for (int i = 0; i < animatingChars.Length; ++i)
                     {
-                        char nextChar = this.Text[i];
+                        char nextChar = animatingChars[i];
 
                         if (shouldSkip(nextChar))
                         {
@@ -46,11 +55,15 @@ public class TextAnimator : VoBehavior
                         }
                         else
                         {
-                            string addition = "";
+                            char addition = ' ';
 
                             if (isAlphabetical(nextChar))
                             {
                                 addition = ALPHABETICAL_CHARACTERS[Random.Range(0, ALPHABETICAL_CHARACTERS.Length)];
+                            }
+                            else if (isPunctuation(nextChar))
+                            {
+                                addition = PUNCTUATION_CHARACTERS[Random.Range(0, PUNCTUATION_CHARACTERS.Length)];
                             }
                             else // Special characters
                             {
@@ -94,11 +107,47 @@ public class TextAnimator : VoBehavior
                 _timeSinceSwap = _swapDuration + 0.1f;
             }
         }
+
+        // If not running, blink the cursor at the end of the text
+        else if (this.EnableCursorBlink)
+        {
+            if (_blinking)
+            {
+                if (_timeSinceBlink >= this.CursorBlinkOnDuration)
+                {
+                    _timeSinceBlink = 0.0f;
+                    _textComponent.text = _textComponent.text.Remove(_textComponent.text.Length - this.CursorText.Length);
+                    _blinking = false;
+                }
+                else
+                {
+                    _timeSinceBlink += Time.deltaTime;
+                }
+            }
+            else
+            {
+                if (_timeSinceBlink >= this.CursorBlinkOffDuration)
+                {
+                    _timeSinceBlink = 0.0f;
+                    _textComponent.text += this.CursorText;
+                    _blinking = true;
+                }
+                else
+                {
+                    _timeSinceBlink += Time.deltaTime;
+                }
+            }
+        }
 	}
 
     public void Begin()
     {
-        _textComponent = this.GetComponent<Text>();
+        if (_blinking)
+        {
+            _textComponent.text.Remove(_textComponent.text.Length - this.CursorText.Length);
+            _blinking = false;
+        }
+
         _swapDuration = 1.0f / (float)this.SwapsPerSecond;
         _singleCharacterDuration = 1.0f / (float)this.CharactersPerSecond;
         _textSoFar = "";
@@ -106,13 +155,15 @@ public class TextAnimator : VoBehavior
         _running = true;
         _timeSinceCharacter = 0;
         _timeSinceSwap = _swapDuration + 0.1f;
+        _timeSinceBlink = 0.0f;
     }
 
     /**
      * Private
      */
-    private static string[] ALPHABETICAL_CHARACTERS = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-    private static string[] SPECIAL_CHARACTERS = { ".", ",", ";", "!", "?", "<", ">", "*", "@", "#", "$", "%", "^", "&", "(", ")", "-", "_", "+", "=", "/", "'", "[", "]", "|", "\"" };
+    private static char[] ALPHABETICAL_CHARACTERS = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+    private static char[] PUNCTUATION_CHARACTERS = { '.', ',', ';', '!', '?', '-', '\'', ':' };
+    private static char[] SPECIAL_CHARACTERS = { '<', '>', '*', '@', '#', '$', '%', '^', '&', '(', ')', '_', '+', '=', '/', '[', ']', '|', '"' };
 
     private Text _textComponent;
     private string _textSoFar;
@@ -122,10 +173,22 @@ public class TextAnimator : VoBehavior
     private float _timeSinceSwap;
     private float _timeSinceCharacter;
     private bool _running;
+    private bool _blinking;
+    private float _timeSinceBlink;
 
     private bool isAlphabetical(char check)
     {
-        return (check >= 'A' && check <= 'Z') || (check >= 'a' && check <= 'z');
+        return (check >= 'A' && check <= 'Z') || (check >= 'a' && check <= 'z') || (check >= '0' && check <= '9');
+    }
+
+    private bool isPunctuation(char check)
+    {
+        foreach (char c in PUNCTUATION_CHARACTERS)
+        {
+            if (c == check)
+                return true;
+        }
+        return false;
     }
 
     private bool shouldSkip(char check)
