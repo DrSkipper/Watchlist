ff59c3d2a338b8c00e3531b10ca0b919c93a443a
Fletcher <fletcherc89@gmail.com>
2015-09-15 22:01:44 -0700

    Two approaches to rendering level select scene

---
+++ Assets/Watchlist/Editor/LevelSelectColorizerEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelSelectColorizer))]
+public class LevelSelectColorizerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUI.changed)
+        {
+            ((LevelSelectColorizer)target).UpdateColor();
+        }
+    }
+}
---
+++ Assets/Watchlist/Scripts/LevelSelectColorizer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LevelSelectColorizer : VoBehavior
+{
+    public Color Color = Color.white;
+    
+    void Start()
+    {
+        this.UpdateColor();
+    }
+
+    public void UpdateColor()
+    {
+        foreach (LineRenderer line in this.gameObject.GetComponentsInChildren<LineRenderer>())
+        {
+            line.SetColors(this.Color, this.Color);
+        }
+    }
+}
