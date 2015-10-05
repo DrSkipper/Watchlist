194c311847b3e50aee1f0d442e03a5f9d5e456ce
Fletcher <fletcherc89@gmail.com>
2015-10-04 20:56:05 -0700

    Fixed a number if problems with collision, but not over yet!

--- Assets/Watchlist/Scripts/Actor2D.cs
+++ Assets/Watchlist/Scripts/Actor2D.cs
@@ -32,38 +32,48 @@ public class Actor2D : VoBehavior
         }
 
         Vector2 soFar = Vector2.zero;
+        Vector2 projected = Vector2.zero;
         Vector2 oldVelocity = this.Velocity;
-        
+        float dMagnitude = d.magnitude;
+
+        bool haltX = false;
+        bool haltY = false;
+
         while (true)
         {
-            bool haltX = moveX(incX, _collisionsFromMove);
-            bool haltY = moveY(incY, _collisionsFromMove);
+            projected.x += incX;
+            projected.y += incY;
 
-            if (haltX || haltY)
+            if (projected.magnitude > dMagnitude)
             {
-                this.Velocity = Vector2.zero;
+                if (!haltX) moveX(d.x - soFar.x, _collisionsFromMove, _horizontalCollisions);
+                if (!haltY) moveY(d.y - soFar.y, _collisionsFromMove, _verticalCollisions);
                 break;
             }
+
+            if (!haltX) haltX = moveX(incX, _collisionsFromMove, _horizontalCollisions);
+            if (!haltY) haltY = moveY(incY, _collisionsFromMove, _verticalCollisions);
+
+            if (haltX && haltY)
+                break;
 
             soFar.x += incX;
             soFar.y += incY;
 
             Vector2 difference = d - soFar;
-            if (difference.magnitude == 0.0f)
+            if (soFar.magnitude >= dMagnitude)
                 break;
-
-            if (soFar.magnitude > d.magnitude)
-            {
-                moveX(d.x - soFar.x, _collisionsFromMove);
-                moveY(d.y - soFar.y, _collisionsFromMove);
-                break;
-            }
         }
+
+        if (haltX) this.Velocity.x = 0;
+        if (haltY) this.Velocity.y = 0;
 
         if (_collisionsFromMove.Count > 0)
         {
             this.localNotifier.SendEvent(new CollisionEvent(_collisionsFromMove.ToArray(), oldVelocity));
             _collisionsFromMove.Clear();
+            _horizontalCollisions.Clear();
+            _verticalCollisions.Clear();
         }
     }
 
@@ -72,9 +82,11 @@ public class Actor2D : VoBehavior
      */
     private Vector2 _positionModifier = Vector2.zero;
     private List<GameObject> _collisionsFromMove = new List<GameObject>();
+    private List<GameObject> _horizontalCollisions = new List<GameObject>();
+    private List<GameObject> _verticalCollisions = new List<GameObject>();
     
     // Returns true if movement was halted
-    private bool moveX(float dx, List<GameObject> collisions)
+    private bool moveX(float dx, List<GameObject> collisions, List<GameObject> horizontalCollisions)
     {
         _positionModifier.x += dx;
         int unitMove = Mathf.RoundToInt(_positionModifier.x);
@@ -86,15 +98,21 @@ public class Actor2D : VoBehavior
 
             while (unitMove != 0)
             {
-                GameObject collidedObject = this.boxCollider2D.CollideFirst(unitDir, 0, this.CollisionMask);
+                int oldCount = horizontalCollisions.Count;
+                this.boxCollider2D.Collide(horizontalCollisions, unitDir, 0, this.CollisionMask);
 
-                if (collidedObject)
+                if (horizontalCollisions.Count > oldCount)
                 {
-                    collisions.Add(collidedObject);
-                    if ((collidedObject.layer & this.HaltMovementMask) != 0)
+                    for (int i = oldCount; i < horizontalCollisions.Count; ++i)
                     {
-                        _positionModifier.x = 0.0f;
-                        return true;
+                        if (!collisions.Contains(horizontalCollisions[i]))
+                            collisions.Add(horizontalCollisions[i]);
+
+                        if (((1 << horizontalCollisions[i].layer) & this.HaltMovementMask) != 0)
+                        {
+                            _positionModifier.x = 0.0f;
+                            return true;
+                        }
                     }
                 }
 
@@ -106,7 +124,7 @@ public class Actor2D : VoBehavior
         return false;
     }
     
-    private bool moveY(float dy, List<GameObject> collisions)
+    private bool moveY(float dy, List<GameObject> collisions, List<GameObject> verticalCollisions)
     {
         _positionModifier.y += dy;
         int unitMove = Mathf.RoundToInt(_positionModifier.y);
@@ -118,15 +136,21 @@ public class Actor2D : VoBehavior
 
             while (unitMove != 0)
             {
-                GameObject collidedObject = this.boxCollider2D.CollideFirst(0, unitDir, this.CollisionMask);
+                int oldCount = verticalCollisions.Count;
+                this.boxCollider2D.Collide(verticalCollisions, 0, unitDir, this.CollisionMask);
 
-                if (collidedObject)
+                if (verticalCollisions.Count > oldCount)
                 {
-                    collisions.Add(collidedObject);
-                    if ((collidedObject.layer & this.HaltMovementMask) != 0)
+                    for (int i = oldCount; i < verticalCollisions.Count; ++i)
                     {
-                        _positionModifier.y = 0.0f;
-                        return true;
+                        if (!collisions.Contains(verticalCollisions[i]))
+                            collisions.Add(verticalCollisions[i]);
+
+                        if (((1 << collisions[i].layer) & this.HaltMovementMask) != 0)
+                        {
+                            _positionModifier.y = 0.0f;
+                            return true;
+                        }
                     }
                 }
 
--- Assets/Watchlist/Scripts/Collider2DExtensions.cs
+++ Assets/Watchlist/Scripts/Collider2DExtensions.cs
@@ -1,30 +1,25 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public static class Collider2DExtensions
 {
-    public const int MIN_OVERLAP_AREA_CHECK = 30;
+    public const int MIN_OVERLAP_AREA_CHECK = 300;
     
     public static int LeftX(this BoxCollider2D self) { return Mathf.RoundToInt(self.bounds.min.x); }
     public static int RightX(this BoxCollider2D self) { return Mathf.RoundToInt(self.bounds.max.x); }
     public static int TopY(this BoxCollider2D self) { return Mathf.RoundToInt(self.bounds.max.y); }
     public static int BottomY(this BoxCollider2D self) { return Mathf.RoundToInt(self.bounds.min.y); }
     
-    public static GameObject CollideFirst(this BoxCollider2D self, int offsetX = 0, int offsetY = 0, int layerMask = Physics2D.DefaultRaycastLayers, string objectTag = null)
+    public static GameObject CollideFirst(this BoxCollider2D self, int offsetX = 0, int offsetY = 0, int layerMask = Physics2D.DefaultRaycastLayers, string objectTag = null, Collider2D[] potentialCollisions = null)
     {
-        // Overlap an area significantly larger than our bounding box so we can directly compare bounds with collision candidates
-        // (Relying purely on OverlapAreaAll for collision seems to be inconsistent at times)
-        //NOTE - There appears to be a bug with OverlapAreaAll when the area checked against is too small
-        // http://answers.unity3d.com/questions/772941/physics2doverlapareaallnoalloc-fails-detecting-obj.html
-        Bounds bounds = self.bounds;
-        float overlapSizeX = Mathf.Max(bounds.size.x, MIN_OVERLAP_AREA_CHECK);
-        float overlapSizeY = Mathf.Max(bounds.size.y, MIN_OVERLAP_AREA_CHECK);
-        Vector2 corner1 = new Vector2(bounds.min.x - overlapSizeX + offsetX, bounds.min.y - overlapSizeY + offsetY);
-        Vector2 corner2 = new Vector2(bounds.max.x + overlapSizeX + offsetX, bounds.max.y + overlapSizeY + offsetY);
-        Collider2D[] colliders = Physics2D.OverlapAreaAll(corner1, corner2, layerMask);
+        if (potentialCollisions == null)
+            potentialCollisions = self.GetPotentialCollisions(0, 0, offsetX, offsetY, layerMask);
 
         // If there is only one collider, it is our collider, so there is nothing to collide with
-        if (colliders.Length <= 1)
+        if (potentialCollisions.Length <= 1)
             return null;
+        
+        Bounds bounds = self.bounds;
 
         // Apply offset to our bounds and make sure we're using integer/pixel-perfect math
         bounds.center = new Vector3(Mathf.Round(bounds.center.x) + offsetX, Mathf.Round(bounds.center.y) + offsetY, Mathf.Round(bounds.center.z));
@@ -32,7 +27,7 @@ public static class Collider2DExtensions
         // Account for bounds intersections marking true when colliders end at same point
         bounds.size = new Vector3(bounds.size.x - 2, bounds.size.y - 2, bounds.size.z);
 
-        foreach (Collider2D collider in colliders)
+        foreach (Collider2D collider in potentialCollisions)
         {
             if (collider != self && (objectTag == null || collider.tag == objectTag))
             {
@@ -46,6 +41,57 @@ public static class Collider2DExtensions
         }
 
         return null;
+    }
+
+    public static void Collide(this BoxCollider2D self, List<GameObject> collisions, int offsetX = 0, int offsetY = 0, int layerMask = Physics2D.DefaultRaycastLayers, string objectTag = null, Collider2D[] potentialCollisions = null)
+    {
+        if (potentialCollisions == null)
+            potentialCollisions = self.GetPotentialCollisions(0, 0, offsetX, offsetY, layerMask);
+
+        // If there is only one collider, it is our collider, so there is nothing to collide with
+        if (potentialCollisions.Length <= 1)
+            return;
+
+        Bounds bounds = self.bounds;
+
+        // Apply offset to our bounds and make sure we're using integer/pixel-perfect math
+        bounds.center = new Vector3(Mathf.Round(bounds.center.x) + offsetX, Mathf.Round(bounds.center.y) + offsetY, Mathf.Round(bounds.center.z));
+
+        // Account for bounds intersections marking true when colliders end at same point
+        bounds.size = new Vector3(bounds.size.x - 2, bounds.size.y - 2, bounds.size.z);
+
+        foreach (Collider2D collider in potentialCollisions)
+        {
+            if (collider != self && (objectTag == null || collider.tag == objectTag))
+            {
+                // Make sure we're using integer/pixel-perfect math
+                Bounds otherBounds = collider.bounds;
+                otherBounds.center = new Vector3(Mathf.Round(otherBounds.center.x), Mathf.Round(otherBounds.center.y), Mathf.Round(otherBounds.center.z));
+
+                if (bounds.Intersects(otherBounds))
+                {
+                    GameObject collidedObject = collider.gameObject;
+                    if (!collisions.Contains(collidedObject))
+                        collisions.Add(collidedObject);
+                }
+            }
+        }
+
+        return;
+    }
+
+    public static Collider2D[] GetPotentialCollisions(this BoxCollider2D self, float vx, float vy, int offsetX = 0, int offsetY = 0, int layerMask = Physics2D.DefaultRaycastLayers)
+    {
+        // Overlap an area significantly larger than our bounding box so we can directly compare bounds with collision candidates
+        // (Relying purely on OverlapAreaAll for collision seems to be inconsistent at times)
+        //NOTE - There appears to be a bug with OverlapAreaAll when the area checked against is too small
+        // http://answers.unity3d.com/questions/772941/physics2doverlapareaallnoalloc-fails-detecting-obj.html
+        Bounds bounds = self.bounds;
+        float overlapSizeX = Mathf.Max(bounds.size.x + Mathf.Abs(vx), MIN_OVERLAP_AREA_CHECK);
+        float overlapSizeY = Mathf.Max(bounds.size.y + Mathf.Abs(vy), MIN_OVERLAP_AREA_CHECK);
+        Vector2 corner1 = new Vector2(bounds.min.x - overlapSizeX + offsetX, bounds.min.y - overlapSizeY + offsetY);
+        Vector2 corner2 = new Vector2(bounds.max.x + overlapSizeX + offsetX, bounds.max.y + overlapSizeY + offsetY);
+        return Physics2D.OverlapAreaAll(corner1, corner2, layerMask);
     }
 
     public static bool CollideCheck(this BoxCollider2D self, GameObject checkObject, int offsetX = 0, int offsetY = 0)
