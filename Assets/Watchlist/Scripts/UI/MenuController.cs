62d6f0e1b250ab4c683b44073ce3301ad1a9af04
Fletcher <fletcherc89@gmail.com>
2015-11-18 16:23:07 -0800

    Some menu highlighting and animations

---
+++ Assets/Watchlist/Scripts/UI/MenuController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class MenuController : VoBehavior
+{
+    public GameObject[] MenuElements;
+    public int CurrentElement = 0;
+
+    void Start()
+    {
+        _animators = new Animator[this.MenuElements.Length];
+        _locks = new bool[this.MenuElements.Length];
+
+        for (int i = 0; i < this.MenuElements.Length; ++i)
+        {
+            Animator animator = this.MenuElements[i].GetComponent<Animator>();
+            bool locked = this.MenuElements[i].GetComponent<MenuElement>().Locked;
+            _animators[i] = animator;
+            _locks[i] = locked;
+            animator.SetBool("Locked", locked);
+        }
+
+        highlightElement(this.CurrentElement);
+    }
+
+    void Update()
+    {
+        if (!_animators[this.CurrentElement].GetCurrentAnimatorStateInfo(0).IsName(_locks[this.CurrentElement] ? "Selected (Locked)" : "Selected (UnLocked"))
+        {
+            if (MenuInput.HighlightNextElement())
+                highlightElement((this.CurrentElement + 1) % this.MenuElements.Length);
+            else if (MenuInput.HighlightPreviousElement())
+                highlightElement(this.CurrentElement == 0 ? this.MenuElements.Length - 1 : this.CurrentElement - 1);
+            else if (MenuInput.SelectCurrentElement())
+                selectCurrentElement();
+        }
+    }
+
+    /**
+     * Private
+     */
+    private Animator[] _animators;
+    private bool[] _locks;
+
+    private void highlightElement(int nextElement)
+    {
+        if (nextElement != this.CurrentElement)
+            _animators[this.CurrentElement].SetTrigger("UnHighlighted");
+
+        this.CurrentElement = nextElement;
+        _animators[this.CurrentElement].SetTrigger("Highlighted");
+    }
+
+    private void selectCurrentElement()
+    {
+        _animators[this.CurrentElement].SetTrigger("Selected");
+    }
+}
---
+++ Assets/Watchlist/Scripts/UI/MenuInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class MenuInput
+{
+    public static bool HighlightNextElement()
+    {
+        return Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S);
+    }
+
+    public static bool HighlightPreviousElement()
+    {
+        return Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
+    }
+
+    public static bool SelectCurrentElement()
+    {
+        return Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space);
+    }
+}
