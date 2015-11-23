203841d35710d97163986cd5123d1f5623eefb95
Fletcher <fletcherc89@gmail.com>
2015-11-23 14:06:29 -0800

    Animating pickups

---
+++ Assets/Watchlist/Scripts/RandomAnimationOffsetter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RandomAnimationOffsetter : MonoBehaviour
+{
+	void Awake()
+    {
+        this.GetComponent<Animator>().SetFloat("Offset", Random.Range(0.0f, 1.0f));
+    }
+}
