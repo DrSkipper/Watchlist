d145303943cde6332ac9470c4d44c1d8e46abda0
Fletcher <fletcherc89@gmail.com>
2016-01-07 00:19:09 -0800

    Level intro animation

---
+++ Assets/Watchlist/Scripts/LevelStartFlow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelStartFlow : VoBehavior
+{
+    public GameObject LevelIntroPanel;
+    public GameObject LevelIntroImage;
+    public float IntroDelay = 1.0f;
+    public float LevelIntroScreenLength = 3.0f;
+
+    void Start()
+    {
+        this.GetComponent<TimedCallbacks>().AddCallback(this, this.LevelStartIn, this.IntroDelay);
+    }
+
+    public void LevelStartIn()
+    {
+        this.LevelIntroPanel.GetComponent<Animator>().SetTrigger("LevelStartIn");
+        this.GetComponent<TimedCallbacks>().AddCallback(this, this.LevelStartOut, this.LevelIntroScreenLength);
+    }
+
+    public void LevelStartOut()
+    {
+        this.LevelIntroPanel.GetComponent<Animator>().SetTrigger("LevelStartOut");
+    }
+}
