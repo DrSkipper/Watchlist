03f9fce0d53b4b54470669cd29728af8962b20b4
Fletcher <fletcherc89@gmail.com>
2016-02-08 22:07:39 -0800

    Started work on Boss Young

---
+++ Assets/Watchlist/Scripts/AccelerateTowards.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Actor2D))]
+public class AccelerateTowards : VoBehavior
+{
+    public float AccelerationDuration = 0.4f;
+    public float MaxSpeed = 260.0f;
+    public float RangeToReachTarget = 10.0f;
+    public ReachedTargetCallback OnReachedTarget;
+
+    public delegate void ReachedTargetCallback(GameObject go);
+
+    void Awake()
+    {
+        _actor = this.GetComponent<Actor2D>();
+        _acceleration = this.AccelerationDuration > 0 ? this.MaxSpeed / this.AccelerationDuration : this.MaxSpeed * 1000;
+    }
+
+    public void SeekTarget(Vector2 target)
+    {
+        _target = target;
+    }
+
+    public void SeekTarget(Vector2 target, ReachedTargetCallback callback)
+    {
+        this.OnReachedTarget = callback;
+        this.SeekTarget(target);
+    }
+
+    public void StopSeeking()
+    {
+        _target = null;
+    }
+
+    void Update()
+    {
+        Vector2 targetVelocity = Vector2.zero;
+        float speed = _actor.Velocity.magnitude;
+
+        if (_target.HasValue)
+        {
+            targetVelocity = _target.Value - (Vector2)this.transform.position;
+            targetVelocity.Normalize();
+            targetVelocity *= this.MaxSpeed;
+        }
+        
+        float changeX = targetVelocity.x - _actor.Velocity.x;
+        float changeY = targetVelocity.y - _actor.Velocity.y;
+
+        if (changeX != 0 || changeY != 0)
+        {
+            float changeTotal = Mathf.Sqrt(Mathf.Pow(changeX, 2) + Mathf.Pow(changeY, 2));
+
+            if (changeX != 0)
+            {
+                float ax = Mathf.Abs(_acceleration * changeX / changeTotal);
+                _actor.Velocity.x = Mathf.Lerp(_actor.Velocity.x, targetVelocity.x, ax * Time.deltaTime / Mathf.Abs(changeX));
+            }
+
+            if (changeY != 0)
+            {
+                float ay = Mathf.Abs(_acceleration * changeY / changeTotal);
+                _actor.Velocity.y = Mathf.Lerp(_actor.Velocity.y, targetVelocity.y, ay * Time.deltaTime / Mathf.Abs(changeY));
+            }
+        }
+
+        if (_target.HasValue && Vector2.Distance(_target.Value, this.transform.position) <= this.RangeToReachTarget)
+        {
+            this.OnReachedTarget(this.gameObject);
+        }
+    }
+
+    /**
+     * Private
+     */
+    private float _acceleration;
+    private Vector2? _target;
+    private Actor2D _actor;
+}
---
+++ Assets/Watchlist/Scripts/Bosses/BossYoungMainBehavior.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BossYoungMainBehavior : VoBehavior
+{
+    public List<GameObject> SubBosses;
+    public GameObject EndFlowObject;
+
+    void Start()
+    {
+        for (int i = 0; i < this.SubBosses.Count; ++i)
+        {
+            GameObject subBoss = this.SubBosses[i];
+            subBoss.GetComponent<Damagable>().OnDeathCallbacks.Add(this.SubBossKilled);
+        }
+
+        _stateMachine.AddState(ROTATION_STATE, updateRotation, enterRotation, exitRotation);
+        _stateMachine.AddState(ATTACKING_STATE, updateAttacking, enterAttacking, exitAttacking);
+        enterRotation();
+        _stateMachine.BeginWithInitialState(ROTATION_STATE);
+    }
+
+    void Update()
+    {
+        _stateMachine.Update();
+    }
+
+    /**
+     * Private
+     */
+    private TimedCallbacks _timedCallbacks;
+    private FSMStateMachine _stateMachine;
+    private bool _switchState;
+
+    private const string ROTATION_STATE = "rotation";
+    private const string ATTACKING_STATE = "path";
+
+    private void switchState()
+    {
+        _switchState = true;
+    }
+
+    private void SubBossKilled(Damagable died)
+    {
+        this.SubBosses.Remove(died.gameObject);
+
+        if (this.SubBosses.Count == 0)
+        {
+            this.EndFlowObject.GetComponent<WinCondition>().EndLevel();
+        }
+    }
+
+    private string updateRotation()
+    {
+        return !_switchState ? ROTATION_STATE : ATTACKING_STATE;
+    }
+
+    private void enterRotation()
+    {
+        _switchState = false;
+    }
+
+    private void exitRotation()
+    {
+    }
+
+    private string updateAttacking()
+    {
+        return !_switchState ? ATTACKING_STATE : ROTATION_STATE;
+    }
+
+    private void enterAttacking()
+    {
+        _switchState = false;
+    }
+
+    private void exitAttacking()
+    {
+    }
+}
