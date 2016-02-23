3106dae9987da86a82f69de6b804ad8f588b0efe
Fletcher <fletcherc89@gmail.com>
2016-02-23 14:38:17 -0800

    Boss Young finished

--- Assets/Watchlist/Scripts/Bosses/BossYoungMainBehavior.cs
+++ Assets/Watchlist/Scripts/Bosses/BossYoungMainBehavior.cs
@@ -4,7 +4,17 @@ using System.Collections.Generic;
 public class BossYoungMainBehavior : VoBehavior
 {
     public List<GameObject> SubBosses;
+    public List<GameObject> SubBossGroups;
+    public Transform AttackBoxLowerLeft;
+    public Transform AttackBoxUpperRight;
+    public float GroupRotationAmount = 90.0f;
+    public float AttackDuration = 4.0f;
     public GameObject EndFlowObject;
+
+    void Awake()
+    {
+        _timedCallbacks = this.GetComponent<TimedCallbacks>();
+    }
 
     void Start()
     {
@@ -12,10 +22,18 @@ public class BossYoungMainBehavior : VoBehavior
         {
             GameObject subBoss = this.SubBosses[i];
             subBoss.GetComponent<Damagable>().OnDeathCallbacks.Add(this.SubBossKilled);
+            subBoss.GetComponent<BossYoungSubBehavior>().ReturnHomeCallback = returnFinished;
         }
 
+        foreach (GameObject group in this.SubBossGroups)
+        {
+            group.GetComponent<LerpRotation>().AddCallback(this.rotationOver);
+        }
+
+        _stateMachine = new FSMStateMachine();
         _stateMachine.AddState(ROTATION_STATE, updateRotation, enterRotation, exitRotation);
         _stateMachine.AddState(ATTACKING_STATE, updateAttacking, enterAttacking, exitAttacking);
+        _stateMachine.AddState(RETURNING_STATE, updateReturning, enterReturning, exitReturning);
         enterRotation();
         _stateMachine.BeginWithInitialState(ROTATION_STATE);
     }
@@ -31,9 +49,12 @@ public class BossYoungMainBehavior : VoBehavior
     private TimedCallbacks _timedCallbacks;
     private FSMStateMachine _stateMachine;
     private bool _switchState;
+    private int _rotationsOver;
+    private int _returnsComplete;
 
     private const string ROTATION_STATE = "rotation";
     private const string ATTACKING_STATE = "path";
+    private const string RETURNING_STATE = "return";
 
     private void switchState()
     {
@@ -48,6 +69,9 @@ public class BossYoungMainBehavior : VoBehavior
         {
             this.EndFlowObject.GetComponent<WinCondition>().EndLevel();
         }
+
+        else if (_stateMachine.CurrentState == RETURNING_STATE)
+            checkReturnsComplete();
     }
 
     private string updateRotation()
@@ -58,6 +82,21 @@ public class BossYoungMainBehavior : VoBehavior
     private void enterRotation()
     {
         _switchState = false;
+        _rotationsOver = 0;
+
+        foreach (GameObject group in this.SubBossGroups)
+        {
+            LerpRotation lerpRot = group.GetComponent<LerpRotation>();
+            lerpRot.TargetRotation = group.GetComponent<Rotation>().GetAngle() + this.GroupRotationAmount;
+            lerpRot.LerpToTargetRotation();
+        }
+    }
+
+    private void rotationOver(GameObject go)
+    {
+        ++_rotationsOver;
+        if (_rotationsOver >= this.SubBossGroups.Count)
+            _switchState = true;
     }
 
     private void exitRotation()
@@ -66,15 +105,59 @@ public class BossYoungMainBehavior : VoBehavior
 
     private string updateAttacking()
     {
-        return !_switchState ? ATTACKING_STATE : ROTATION_STATE;
+        return !_switchState ? ATTACKING_STATE : RETURNING_STATE;
     }
 
     private void enterAttacking()
     {
         _switchState = false;
+
+        foreach (GameObject subBoss in this.SubBosses)
+        {
+            subBoss.GetComponent<BossYoungSubBehavior>().BeginAttackPathing(new IntegerVector(this.AttackBoxLowerLeft.position), new IntegerVector(this.AttackBoxUpperRight.position));
+        }
+
+        _timedCallbacks.AddCallback(this, attackingDone, this.AttackDuration);
     }
 
     private void exitAttacking()
     {
+    }
+
+    private void attackingDone()
+    {
+        _switchState = true;
+    }
+
+    private string updateReturning()
+    {
+        return !_switchState ? RETURNING_STATE : ROTATION_STATE;
+    }
+
+    private void enterReturning()
+    {
+        _switchState = false;
+        _returnsComplete = 0;
+
+        foreach (GameObject subBoss in this.SubBosses)
+        {
+            subBoss.GetComponent<BossYoungSubBehavior>().StopAttackingAndReturnHome();
+        }
+    }
+
+    private void exitReturning()
+    {
+    }
+
+    private void returnFinished(GameObject go)
+    {
+        ++_returnsComplete;
+        checkReturnsComplete();
+    }
+
+    private void checkReturnsComplete()
+    {
+        if (_returnsComplete >= this.SubBosses.Count)
+            _switchState = true;
     }
 }
---
+++ Assets/Watchlist/Scripts/Bosses/BossYoungSubBehavior.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class BossYoungSubBehavior : VoBehavior
+{
+    public LerpMovement.LerpFinishedCallback ReturnHomeCallback;
+    public float PreferredMinDistance = 200.0f;
+    public int AttemptsAtPreferred = 3;
+
+    void Awake()
+    {
+        _lerpMovement = this.GetComponent<LerpMovement>();
+    }
+
+    void Start()
+    {
+        _home = new IntegerVector(this.transform.position);
+        _lerpMovement.AddCallback(movementFinished);
+    }
+
+    public void BeginAttackPathing(IntegerVector lowerLeft, IntegerVector upperRight)
+    {
+        _attacking = true;
+        _lowerLeft = lowerLeft;
+        _upperRight = upperRight;
+        seekAttackPoint();
+    }
+
+    public void StopAttackingAndReturnHome()
+    {
+        _attacking = false;
+        _lerpMovement.BeginMovement(_home);
+    }
+
+    /**
+     * Private
+     */
+    private IntegerVector _lowerLeft;
+    private IntegerVector _upperRight;
+    private IntegerVector _home;
+    private LerpMovement _lerpMovement;
+    private bool _attacking;
+
+    private void seekAttackPoint()
+    {
+        IntegerVector target = new IntegerVector(Random.Range(_lowerLeft.X, _upperRight.X), Random.Range(_lowerLeft.Y, _upperRight.Y));
+
+        int i = 0;
+        while (i < this.AttemptsAtPreferred && Vector2.Distance(this.transform.position, target) < this.PreferredMinDistance)
+        {
+            target = new IntegerVector(Random.Range(_lowerLeft.X, _upperRight.X), Random.Range(_lowerLeft.Y, _upperRight.Y));
+            ++i;
+        }
+        
+        _lerpMovement.BeginMovement(target);
+    }
+
+    private void movementFinished(GameObject go)
+    {
+        if (_attacking)
+            seekAttackPoint();
+        else if (this.ReturnHomeCallback != null)
+            this.ReturnHomeCallback(go);
+    }
+}
