e40b5d6e418e8630cf48f7223fcfc699bab97647
Fletcher <fletcherc89@gmail.com>
2016-02-23 17:49:00 -0800

    Boss Elder progress

---
+++ Assets/Watchlist/Scripts/RadialDistancing.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RadialDistancing : VoBehavior
+{
+    [System.Serializable]
+    public struct RadialElement
+    {
+        public Transform transform;
+        public float desiredAngle;
+    }
+
+    public List<RadialElement> RadialElements;
+    public float Radius = 300.0f;
+    public float RadiusChangeSpeed = -100.0f;
+
+    void Update()
+    {
+        this.Radius += this.RadiusChangeSpeed * Time.deltaTime;
+
+        foreach (RadialElement element in this.RadialElements)
+        {
+            element.transform.localPosition = new Vector3(this.Radius * Mathf.Cos(element.desiredAngle * Mathf.Deg2Rad), this.Radius * Mathf.Sin(element.desiredAngle * Mathf.Deg2Rad), element.transform.localPosition.z);
+        }
+    }
+}
