1c7fabd55d77f65011fe973037612a13d90c652e
Fletcher <fletcherc89@gmail.com>
2016-03-01 18:34:03 -0800

    Staring code for Level Gen Inpu

---
+++ Assets/Watchlist/Scripts/LevelGenerator/LevelGenInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LevelGenInput : ScriptableObject
+{
+    public enum GenerationType
+    {
+        CA,
+        BSP,
+        Room
+    }
+
+    public GenerationType Type;
+    public IntegerVector NumEnemiesRange;
+    public int[] GuaranteedEnemiesByDifficulty;
+    public IntegerVector[] MapSizes;
+    public IntegerVector NumRoomsRange;
+    public int MinDistanceBetweenSpawns;
+}
