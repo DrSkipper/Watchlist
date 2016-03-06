2c575380c11588611b41d92844897ffe78c12b52
Fletcher <fletcherc89@gmail.com>
2016-03-05 23:50:54 -0800

    Added 3 columns to EnemyType
    Difficulty, ChoiceWeight, ChoiceWeightIncrease

--- Assets/Watchlist/Scripts/Data/EnemyType.cs
+++ Assets/Watchlist/Scripts/Data/EnemyType.cs
@@ -10,6 +10,9 @@ public class EnemyType
     public int Id;
     public string PrefabName = "generic";
     public string SpriteName = "enemy_02";
+    public int Difficulty = 0;
+    public int ChoiceWeight = 10;
+    public int ChoiceWeightIncrease = 5;
     public int Health = 5;
     public float Friction = 150.0f;
     public float MaxSpeed = 140.0f;
