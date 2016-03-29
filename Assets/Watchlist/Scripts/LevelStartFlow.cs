68839abc6d821a0489df31c778687f652ea641b7
Fletcher <fletcherc89@gmail.com>
2016-03-28 21:53:12 -0700

    Fixed level start bug in training room

--- Assets/Watchlist/Scripts/LevelStartFlow.cs
+++ Assets/Watchlist/Scripts/LevelStartFlow.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 public class LevelStartFlow : VoBehavior
 {
     public GameObject LevelIntroPanel;
-    public GameObject LevelIntroImage;
     public float IntroDelay = 1.0f;
     public float LevelIntroScreenLength = 3.0f;
 
@@ -15,12 +14,14 @@ public class LevelStartFlow : VoBehavior
 
     public void LevelStartIn()
     {
-        this.LevelIntroPanel.GetComponent<Animator>().SetTrigger("LevelStartIn");
+        if (this.LevelIntroPanel != null)
+            this.LevelIntroPanel.GetComponent<Animator>().SetTrigger("LevelStartIn");
         this.GetComponent<TimedCallbacks>().AddCallback(this, this.LevelStartOut, this.LevelIntroScreenLength);
     }
 
     public void LevelStartOut()
     {
-        this.LevelIntroPanel.GetComponent<Animator>().SetTrigger("LevelStartOut");
+        if (this.LevelIntroPanel != null)
+            this.LevelIntroPanel.GetComponent<Animator>().SetTrigger("LevelStartOut");
     }
 }
