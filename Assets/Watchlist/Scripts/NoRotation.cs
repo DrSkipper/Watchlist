694b55126f3be3656664562fc84558d7d5a92d54
Fletcher <fletcherc89@gmail.com>
2016-05-26 14:29:54 -0700

    Main menu graphical work

---
+++ Assets/Watchlist/Scripts/NoRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class NoRotation : VoBehavior
+{
+    void Start()
+    {
+        this.transform.rotation = Quaternion.identity;
+    }
+
+    void LateUpdate()
+    {
+        this.transform.rotation = Quaternion.identity;
+    }
+}
