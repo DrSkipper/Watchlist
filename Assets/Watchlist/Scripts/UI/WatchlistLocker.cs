3af28350856ed444276e701b7716f4f1adb19b6f
Fletcher <fletcherc89@gmail.com>
2016-05-31 18:36:03 -0700

    dunno git, unity

---
+++ Assets/Watchlist/Scripts/UI/WatchlistLocker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WatchlistLocker : MonoBehaviour
+{
+    public MenuElement MenuElement;
+    public Image ImageToLock;
+    public Sprite LockSprite;
+    public Text TextToLock;
+    public string LockText;
+    public int BossId = 0;
+    public bool ForceLock = false;
+
+    void Awake()
+    {
+        if (this.ForceLock)
+        {
+            this.ImageToLock.sprite = this.LockSprite;
+            this.TextToLock.text = this.LockText;
+        }
+    }
+}
