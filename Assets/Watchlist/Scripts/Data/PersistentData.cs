43d065fa2a0ec1186359e3ca7cee689982400a8c
Fletcher <fletcherc89@gmail.com>
2016-06-01 16:10:35 -0700

    Boss unlocking progress

--- Assets/Watchlist/Scripts/Data/PersistentData.cs
+++ Assets/Watchlist/Scripts/Data/PersistentData.cs
@@ -8,8 +8,7 @@ public static class PersistentData
     
     public static int[] GenerateBossesForPlaythrough()
     {
-        if (_bossLockStatuses == null)
-            LoadFromDisk();
+        LoadFromDisk();
         List<int> possibilities = new List<int>();
 
         foreach (int bossId in _bossLockStatuses.Keys)
@@ -31,9 +30,30 @@ public static class PersistentData
 
     public static bool CheckForLockedBoss(int bossId)
     {
-        if (_bossLockStatuses == null)
-            LoadFromDisk();
+        LoadFromDisk();
         return _bossLockStatuses[bossId];
+    }
+
+    public static void UnlockBoss(int bossId)
+    {
+        LoadFromDisk();
+        _bossLockStatuses[bossId] = true;
+        if (!_bossUnlocksNeedingNotification.Contains(bossId))
+            _bossUnlocksNeedingNotification.Add(bossId);
+        SaveToDisk();
+    }
+
+    public static int[] BossUnlocksNeedingNotification()
+    {
+        LoadFromDisk();
+        return _bossUnlocksNeedingNotification.ToArray();
+    }
+
+    public static void BossUnlockNotified(int bossId)
+    {
+        LoadFromDisk();
+        _bossUnlocksNeedingNotification.Remove(bossId);
+        SaveToDisk();
     }
 
     public static void SaveToDisk()
@@ -61,13 +81,65 @@ public static class PersistentData
             {
                 getInitialBossLockData();
             }
+
+            if (_levelsBeatenByNumPlayers == null)
+                _levelsBeatenByNumPlayers = new int[DynamicData.MAX_PLAYERS];
         }
     }
 
     public static void EraseLocalData()
     {
         getInitialBossLockData();
+        _levelsBeatenByNumPlayers = new int[DynamicData.MAX_PLAYERS];
+        _bossesBeaten = 0;
+        _timesBeat4CornerBosses = 0;
+        _timesAcceptedMaster = 0;
+        _timesRefusedMaster = 0;
+        _timesDefeatedMaster = 0;
+        _timesClearedMap = 0;
         SaveToDisk();
+    }
+
+    public static void RegisterLevelBeaten(int numPlayers)
+    {
+        LoadFromDisk();
+        _levelsBeatenByNumPlayers[numPlayers - 1]++;
+    }
+
+    public static void RegisterBossBeaten()
+    {
+        LoadFromDisk();
+        ++_bossesBeaten;
+    }
+
+    public static void Register4CornerBossesBeaten()
+    {
+        LoadFromDisk();
+        ++_timesBeat4CornerBosses;
+    }
+
+    public static void RegisterAcceptedMaster()
+    {
+        LoadFromDisk();
+        ++_timesAcceptedMaster;
+    }
+
+    public static void RegisterRefusedMaster()
+    {
+        LoadFromDisk();
+        ++_timesRefusedMaster;
+    }
+
+    public static void RegisterDefeatedMaster()
+    {
+        LoadFromDisk();
+        ++_timesDefeatedMaster;
+    }
+
+    public static void RegisterClearedMap()
+    {
+        LoadFromDisk();
+        ++_timesClearedMap;
     }
 
     [System.Serializable]
@@ -75,17 +147,39 @@ public static class PersistentData
     {
         public bool DataSaved;
         public Dictionary<int, bool> BossLockStatuses;
+        public List<int> BossUnlocksNeedingNotification;
+        public int[] LevelsBeatenByNumPlayers;
+        public int BossesBeaten;
+        public int TimesBeat4CornerBosses;
+        public int TimesAcceptedMaster;
+        public int TimesRefusedMaster;
+        public int TimesDefeatedMaster;
+        public int TimesClearedMap;
     }
 
     /**
      * Private
      */
     private static Dictionary<int, bool> _bossLockStatuses;
+    private static List<int> _bossUnlocksNeedingNotification;
+    private static int[] _levelsBeatenByNumPlayers;
+    private static int _bossesBeaten;
+    private static int _timesBeat4CornerBosses;
+    private static int _timesAcceptedMaster;
+    private static int _timesRefusedMaster;
+    private static int _timesDefeatedMaster;
+    private static int _timesClearedMap;
     private static bool _hasLoaded = false;
+
+    private const int BOSS_LOVERS = 1000001;
+    private const int BOSS_COMMUNAL = 1000003;
+    private const int BOSS_EARTH = 1000006;
+    private const int BOSS_ELDER = 1000008;
 
     private static void getInitialBossLockData()
     {
         _bossLockStatuses = new Dictionary<int, bool>();
+        _bossUnlocksNeedingNotification = new List<int>();
         foreach (int key in StaticData.BossLockData.BossLocks.Keys)
         {
             _bossLockStatuses[key] = StaticData.BossLockData.BossLocks[key];
@@ -94,10 +188,62 @@ public static class PersistentData
 
     private static void guaranteeAllBossesPresent()
     {
+        if (_bossLockStatuses == null)
+            _bossLockStatuses = new Dictionary<int, bool>();
+        if (_bossUnlocksNeedingNotification == null)
+            _bossUnlocksNeedingNotification = new List<int>();
         foreach (int key in StaticData.BossLockData.BossLocks.Keys)
         {
             if (!_bossLockStatuses.ContainsKey(key))
                 _bossLockStatuses[key] = StaticData.BossLockData.BossLocks[key];
+        }
+    }
+
+    private static void checkLoversUnlock()
+    {
+        if (!_bossLockStatuses[BOSS_LOVERS])
+        {
+            if (_levelsBeatenByNumPlayers[1] + _levelsBeatenByNumPlayers[2] + _levelsBeatenByNumPlayers[3] >= 3)
+            {
+                UnlockBoss(BOSS_LOVERS);
+                // notify unlock
+            }
+        }
+    }
+
+    private static void checkCommunalUnlock()
+    {
+        if (!_bossLockStatuses[BOSS_COMMUNAL])
+        {
+            if (_levelsBeatenByNumPlayers[2] + _levelsBeatenByNumPlayers[3] > 2)
+            {
+                UnlockBoss(BOSS_COMMUNAL);
+                // notify unlock
+            }
+        }
+    }
+
+    private static void checkEarthUnlock()
+    {
+        if (!_bossLockStatuses[BOSS_EARTH])
+        {
+            if (_timesClearedMap >= 1)
+            {
+                UnlockBoss(BOSS_EARTH);
+                // notify unlock
+            }
+        }
+    }
+
+    private static void checkElderUnlock()
+    {
+        if (!_bossLockStatuses[BOSS_ELDER])
+        {
+            if (_timesBeat4CornerBosses >= 1)
+            {
+                UnlockBoss(BOSS_ELDER);
+                // notify unlock
+            }
         }
     }
 }
