829d762b369ab07a84272bd65d145615f53bbd26
Fletcher <fletcherc89@gmail.com>
2016-10-11 15:30:50 -0700

    Credits and watchlist scene tuneup

--- Assets/Watchlist/Scripts/MusicHandler.cs
+++ Assets/Watchlist/Scripts/MusicHandler.cs
@@ -38,7 +38,11 @@ public class MusicHandler : MonoBehaviour
     void OnLevelWasLoaded(int i)
     {
         if (!this.Destroyer.MarkedForDestruction)
+        {
+            if (_timedCallbacks == null)
+                _timedCallbacks = this.GetComponent<TimedCallbacks>();
             sceneBegin();
+        }
     }
 
     void Update()
