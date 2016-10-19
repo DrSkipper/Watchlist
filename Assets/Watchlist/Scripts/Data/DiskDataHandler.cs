fe645f3d129b572813975ce85d507abe167c4c40
Fletcher <fletcherc89@gmail.com>
2016-10-19 13:15:52 -0700

    cleanup

--- Assets/Watchlist/Scripts/Data/DiskDataHandler.cs
+++ Assets/Watchlist/Scripts/Data/DiskDataHandler.cs
@@ -1,41 +1,17 @@
 ﻿using UnityEngine;
-using System.IO;
-using System.Text;
 using Newtonsoft.Json;
-using System.Xml;
-using System.Xml.Serialization;
 
 public static class DiskDataHandler
 {
-    public static string LoadStreamedAsset(string filePath)
-    {
-        if (filePath.Contains("://")) {
-            WWW www = new WWW(filePath);
-            //yield return www;
-            return www.text;
-        } else
-            return System.IO.File.ReadAllText(filePath);
-    }
-
-
     public static void Save(string path, object data)
     {
-        /*var serializer = new XmlSerializer(data.GetType());
-        using (StringWriter sw = new StringWriter())
-        {
-            serializer.Serialize(sw, data);
-        }*/
-
         PlayerPrefs.SetString(path, JsonConvert.SerializeObject(data));
     }
 
     public static T Load<T>(string path)
     {
-        //string fullPath = Application.persistentDataPath + Path.DirectorySeparatorChar + path;
         if (PlayerPrefs.HasKey(path))
         {
-            /*var serializer = new XmlSerializer(typeof(T));
-            return (T)serializer.Deserialize(new StringReader(PlayerPrefs.GetString(path)));*/
             return JsonConvert.DeserializeObject<T>(PlayerPrefs.GetString(path));
         }
         return default(T);
