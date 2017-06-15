1f13d65666d8d7a49cda409f36dfe1f588605fc1
Fletcher <fletcherc89@gmail.com>
2017-06-15 16:32:19 -0700

    Fixed explosion radius rendering incorrect size for one frame (got to remove an update method too!)

--- Assets/Watchlist/Scripts/CircleRenderer.cs
+++ Assets/Watchlist/Scripts/CircleRenderer.cs
@@ -6,30 +6,12 @@ public class CircleRenderer : VoBehavior
 {
     public int NumPoints = 16;
     public float InitialRadius = 0.0f;
-    public float Radius { get { return _radius; } set { _radius = value; _dirty = true; } }
+    public float Radius { get { return _radius; } set { setRadius(value); _dirty = true; } }
 
     void Start()
     {
         _lines = this.GetComponent<LineRenderer>();
         this.Radius = this.InitialRadius;
-    }
-
-    void Update()
-    {
-        if (_dirty)
-        {
-            _dirty = false;
-
-            float thetaScale = (2.0f * Mathf.PI) / ((float)this.NumPoints);
-            float theta = 0.0f;
-            _lines.SetVertexCount(this.NumPoints + 2);
-
-            for (int i = 0; i < this.NumPoints + 2; ++i)
-            {
-                theta += thetaScale;
-                _lines.SetPosition(i, new Vector3(_radius * Mathf.Cos(theta), _radius * Mathf.Sin(theta), 0));
-            }
-        }
     }
 
     /**
@@ -38,4 +20,18 @@ public class CircleRenderer : VoBehavior
     private LineRenderer _lines;
     private bool _dirty;
     private float _radius;
+
+    private void setRadius(float r)
+    {
+        _radius = r;
+        float thetaScale = (2.0f * Mathf.PI) / ((float)this.NumPoints);
+        float theta = 0.0f;
+        _lines.positionCount = this.NumPoints + 2;
+
+        for (int i = 0; i < this.NumPoints + 2; ++i)
+        {
+            theta += thetaScale;
+            _lines.SetPosition(i, new Vector3(_radius * Mathf.Cos(theta), _radius * Mathf.Sin(theta), 0));
+        }
+    }
 }
