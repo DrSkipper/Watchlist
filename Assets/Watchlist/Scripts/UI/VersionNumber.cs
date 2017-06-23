04315cc8d4bfd9d6acf15582cdf2fe6b787e45db
Fletcher <fletcherc89@gmail.com>
2017-06-23 13:37:15 -0700

    Version number in options screen (1.3.0 to start)

---
+++ Assets/Watchlist/Scripts/UI/VersionNumber.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionNumber : MonoBehaviour
+{
+    public const string PREFIX = "v";
+    public const int MAJOR = 1;
+    public const int MINOR = 3;
+    public const int PATCH = 0;
+    public const string DOT = ".";
+
+    public Text VersionText; 
+
+    public static string FullVersionString
+    {
+        get
+        {
+            return PREFIX + MAJOR + DOT + MINOR + DOT + PATCH;
+        }
+    }
+
+    private void Start()
+    {
+        this.VersionText.text = FullVersionString;
+    }
+}
