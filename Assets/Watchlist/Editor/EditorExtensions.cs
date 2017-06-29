88a55c3e127eedd50671fb36d26a7f03659a62a4
Fletcher <fletcherc89@gmail.com>
2017-06-29 13:45:48 -0700

    Fix default resolution on first startup

---
+++ Assets/Watchlist/Editor/EditorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class EditorExtensions
+{
+    [MenuItem("Edit/Reset Playerprefs")]
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Player prefs wiped");
+    }
+}
---
+++ Assets/Watchlist/Scripts/ResolutionFixer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ResolutionFixer : MonoBehaviour
+{
+    public const string RES_KEY = "cached_res";
+
+#if !UNITY_EDITOR
+    void Awake()
+    {
+        if (!PlayerPrefs.HasKey(RES_KEY) || PlayerPrefs.GetInt(RES_KEY) != 1)
+        {
+            Resolution[] resolutions = Screen.resolutions;
+            Resolution res = resolutions[resolutions.Length - 1];
+            Screen.SetResolution(res.width, res.height, true);
+            PlayerPrefs.SetInt(RES_KEY, 1);
+        }
+    }
+#endif
+}
