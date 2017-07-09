119071c9f96de854a16a280f603b17606fbc7f13
Fletcher <fletcherc89@gmail.com>
2017-07-09 14:20:43 -0700

    Version 1.3.7

--- Assets/Watchlist/Scripts/UI/VersionNumber.cs
+++ Assets/Watchlist/Scripts/UI/VersionNumber.cs
@@ -6,7 +6,7 @@ public class VersionNumber : MonoBehaviour
     public const string PREFIX = "v";
     public const int MAJOR = 1;
     public const int MINOR = 3;
-    public const int PATCH = 6;
+    public const int PATCH = 7;
     public const string DOT = ".";
 
     public Text VersionText; 
